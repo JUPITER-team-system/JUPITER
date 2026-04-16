4c41be58347c4a51dcda4b28a723affdc22ac8c4
TheDarkness6859 <suarezgarcia939@gmail.com>
2026-04-16 07:03:06 -0500

    feat(Main): add instance of repositories and services

--- src/main/java/com/management/jupiter/Main.java
+++ src/main/java/com/management/jupiter/Main.java
@@ -8,6 +8,8 @@ import com.management.jupiter.models.Admin;
 import com.management.jupiter.models.Coder;
 import com.management.jupiter.models.Tl;
 import com.management.jupiter.models.User;
+import com.management.jupiter.repository.*;
+import com.management.jupiter.services.AssignmentService;
 import com.management.jupiter.util.scanner.ScannerUtil;
 import com.management.jupiter.views.AdminView;
 import com.management.jupiter.views.CoderView;
@@ -20,14 +22,28 @@ public class Main {
 
     public static void main (String[] args){
 
+        //Input
         Scanner scanner = new Scanner(System.in);
         ScannerUtil input = new ScannerUtil(scanner);
 
+        //Repositories:
+        AdminRepository adminRepo = new AdminRepository();
+        ClanRepository clanRepo = new ClanRepository();
+        CoderRepository coderRepo = new CoderRepository();
+        TeamLeaderRepository tlRepo = new TeamLeaderRepository();
+        UserRepository userRepo = new UserRepository();
+
+        //Services:
+        AssignmentService assignmentService = new AssignmentService(clanRepo, tlRepo, coderRepo);
+
+
+        //Controllers:
         UserController userController = new UserController();
         AdminController adminController = new AdminController();
         TlController tlController = new TlController();
         CoderController coderController = new CoderController();
 
+        //Views:
         LoginView login = new LoginView(input, userController);
         AdminView admin = new AdminView(input, adminController);
         TlView tl = new TlView(input, tlController);
