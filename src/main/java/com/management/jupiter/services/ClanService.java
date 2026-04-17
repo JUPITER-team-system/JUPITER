f71abf824dc4da908328f50a902d6edd29c275cb
TheDarkness6859 <suarezgarcia939@gmail.com>
2026-04-17 12:14:45 -0500

    feat(ClanService): implement data mapping for repositories

--- src/main/java/com/management/jupiter/services/ClanService.java
+++ src/main/java/com/management/jupiter/services/ClanService.java
@@ -1,4 +1,129 @@
 package com.management.jupiter.services;
 
+import com.management.jupiter.models.*;
+import com.management.jupiter.persistance.DatabaseConnection;
+import com.management.jupiter.repository.ClanRepository;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.*;
+
 public class ClanService {
+
+    private final ClanRepository clanRepo;
+
+    public ClanService (ClanRepository clanRepo) {
+
+        this.clanRepo = clanRepo;
+
+    }
+
+    public List<Clan> readAll () {
+
+        List<Clan> clanList = new ArrayList<>();
+
+        try {
+
+            clanList = clanRepo.findAll();
+
+        } catch (Exception err) {
+
+            System.err.println("Error to obtain clans: " + err.getMessage());
+
+        }
+
+        return clanList;
+
+    }
+
+    public void add (Clan clan) {
+
+        try(Connection conn = DatabaseConnection.getConnection()){
+
+            try {
+
+                conn.setAutoCommit(false);
+
+                UUID data = clanRepo.save(clan, conn);
+
+                for (User coder : clan.getCoders()){
+
+                    clanRepo.addUser(data, coder.getId(), conn);
+
+                }
+
+                for (User tl : clan.getTls()){
+
+                    clanRepo.addUser(data, tl.getId(), conn);
+
+                }
+
+                conn.commit();
+                System.out.println("Users added correctly");
+
+            }catch (SQLException err){
+
+                conn.rollback();
+                System.err.println("Revert transfer:" + err.getMessage());
+
+            }
+
+        }catch (Exception err){
+
+            System.err.println("Error to add Users: " + err.getMessage());
+
+        }
+
+    }
+
+    public void delete (Clan clan) {
+
+        clanRepo.delete(clan.getId());
+
+    }
+
+    public void edit (Clan clan) {
+
+        try(Connection conn = DatabaseConnection.getConnection()){
+
+            try{
+
+                conn.setAutoCommit(false);
+
+                var clanId = clan.getId();
+
+                clanRepo.edit(clan, conn);
+
+                clanRepo.removeUser(clanId, conn);
+
+                for (User coder : clan.getCoders()){
+
+                    clanRepo.addUser(UUID.fromString(clanId), coder.getId(), conn);
+
+                }
+
+                for (User tl : clan.getTls()){
+
+                    clanRepo.addUser(UUID.fromString(clanId), tl.getId(), conn);
+
+                }
+
+                conn.commit();
+
+            }catch (SQLException err){
+
+                conn.rollback();
+                System.err.println("Revert transfer:" + err.getMessage());
+
+            }
+
+        }catch (Exception err){
+
+            System.err.println("Error to add Users: " + err.getMessage());
+
+        }
+
+    }
+
+
 }
